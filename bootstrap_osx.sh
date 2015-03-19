6cc487059ebd23c942e5534117c18de5a79d01e6
bpurgaso <nullrouteftw@gmail.com>
2015-03-19 17:45:20 +0530

    starting to check for os specific stuff in the bootstrap script

--- bootstrap_osx.sh
+++ bootstrap_osx.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Fail if any sub command fails
+set -e
+
 code_dir='~/code/'
 mkdir -p $code_dir
-cd $code
+cd $code_dir
+
+#install dependencies
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo yum install git vim tmux
+elif [["$OSTYPE" == "darwin"* ]]; then
+    brew install git macvim tmux reattach-to-user-namespace
 
 git clone https://github.com/altercation/solarized.git
 git clone https://github.com/bpurgaso/dotfiles.git
@@ -13,7 +22,7 @@ cat $code_dir.zshrc >> ~/.zshrc
 ln -s $code_dir/dotfiles/bunsen.zsh-theme ~/.oh-my-zsh/themes/bunsen.zsh-theme
 
 # vim customizations
-ln -s ~/code/dotfiles/.vimrc ~/.vimrc
+ln -s $code_dir/dotfiles/.vimrc ~/.vimrc
 
 # tmux customizations
-ln -s $code/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s $code_dir/dotfiles/.tmux.conf ~/.tmux.conf
